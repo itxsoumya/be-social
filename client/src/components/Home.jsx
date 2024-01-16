a9ee91bd2872a5146e3d3f9f8080084c0085682d
itxsoumya <itxsoumyaranjan@gmail.com>
2024-01-16 23:43:46 +0530

    frontend-

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -39,7 +39,7 @@ const data = [
 const Post = ({ profile, content, name, username }) => {
     return (
 
-        <div className="p-4 border-b-2 border-zinc-300 hover:bg-slate-50 cursor-pointer flex text-lg">
+        <div className="p-4 border-b-2 border-zinc-300 hover:bg-slate-100 cursor-pointer flex sm:text-xl text-lg">
             <div className='bg-ered-200'>
                 <div className='rounded-full sm:w-16 sm:h-16 h-14 w-14'>
                     <img src={profile} className='rounded-full  sm:h-16 sm:w-16 h-14 w-14 cursor-pointer ' alt="nehi he bhi" />
