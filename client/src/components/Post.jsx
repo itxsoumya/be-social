ba7ffb53608fd8db80890754cfb6f681b3c3fd25
itxsoumya <itxsoumyaranjan@gmail.com>
2024-01-23 07:11:06 +0530

    banner update

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -5,7 +5,7 @@ import { FiShare2 } from "react-icons/fi";
 const Post = ({ profile, content, name, username }) => {
     return (
 
-        <div className="p-4 border-b-2 border-zinc-300 hover:bg-slate-100 cursor-pointer flex sm:text-xl text-base">
+        <div className="p-4 border-b-2 border-zinc-300 hover:bg-slate-100 cursor-pointer flex sm:text-lg text-base">
             <div className='bg-ered-200'>
                 <div className='rounded-full sm:w-16 sm:h-16 h-12 w-12'>
                     <img src={profile} className='rounded-full  sm:h-16 sm:w-16 h-12 w-12 cursor-pointer ' alt="nehi he bhi" />
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -29,19 +29,30 @@ const data = [
 
 ]
 
+// not sure
+const MessageFlotButton = ()=>{
+    return(
+        <div className='fixed bottom-14   right-[9%]'>
+            M
+        </div>
+    )
+}
+
 const Profile = () => {
     return (
         <div className=" sm:border-2 border-zincc-300 max-w-3xl mx-auto bg-red--50 ">
 
             <div className='bg-indigo-400 sm:h-48 h-40'>
                 {/* banner */}
+
+                <img className='sm:h-48 h-40 w-full' src="https://i.pinimg.com/originals/fb/82/f4/fb82f4512599ffe6de89c3c1704b8de8.jpg" alt="" />
             </div>
-            <div className='absolute  -translate-x-1/2 -translate-y-1/2 rounded-full left-1/2 w-32 h-32 sm:w-36 sm:h-36 z-10'>
-                <img className='rounded-full sm:w-36 sm:h-36 w-32 h-32' src={pimage} alt="" />
+            <div className='absolute  -translate-x-1/2 -translate-y-1/2 rounded-full left-1/2 w-32 h-32 sm:w-36 sm:h-36 z-10 shadow-lg'>
+                <img className='rounded-full sm:w-36 sm:h-36 w-32 h-32' src="https://pbs.twimg.com/profile_images/1330311226266689540/gJb2FMcr_400x400.jpg" alt="" />
             </div>
 
             <div className='text-end mt-2 mr-2'>
-                <button className='bg-pink-600 p-2 rounded-full px-4 text-white shadow-lg text-lg'>Follow</button>
+                <button className='bg-zinc-700 p-2 rounded-full px-4 text-white shadow-lg text-base sm:text-lg'>Follow</button>
             </div>
 
             <div className='sm:mt-10 mt-6 text-2xl text-center'>Soumya Ranjan</div>
@@ -53,23 +64,23 @@ const Profile = () => {
                 </div>
             </div>
 
-            <div className='flex bg-gray-100 mt-12 py-4 sm:text-xl text-lg'>
+            <div className='flex bg-grayy-100 mt-10 sm:mt-11 py-4 sm:text-xl text-lg'>
                 <div className='flex-grow flex flex-col justify-center items-center'>
-                    <p>1.9K</p>
-                    <p>FOLLOWERS</p>
+                    <p className='font-semibold'>1.9K</p>
+                    <p className='text-sm'>FOLLOWERS</p>
                     
                 </div>
                 <div className='flex-grow flex flex-col justify-center items-center'>
-                    <p>9.8K</p>
-                    <p>FOLLOWING</p>
+                    <p className='font-semibold'>53.9K</p>
+                    <p className='text-sm'>VIEWS</p>
                 </div>
                 <div className='flex-grow flex flex-col justify-center items-center'>
-                    <p>53.9K</p>
-                    <p>VIEWS</p>
+                    <p className='font-semibold'>9.8K</p>
+                    <p className='text-sm'>FOLLOWING</p>
                 </div>
             </div>
 
-            <div className='p-2 border-b-2'>
+            <div className='p-2 border-b-2 border-t-2 sm:mt-10 mt-7'>
                 All Posts
             </div>
 
@@ -79,6 +90,7 @@ const Profile = () => {
             {
                 data.map(e => <Post name={e.name} username={e.username} content={e.content} profile={e.profile} />)
             }
+            <MessageFlotButton/>
 
 
         </div>
