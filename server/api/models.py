cb8d95c07add0d591157b2dd17ab9a7631f55434
itxsoumya <itxsoumyaranjan@gmail.com>
2024-03-07 06:11:47 +0530

    basic try

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    post_id = models.ForeignKey(Post,on_delete=models.DO_NOTHING)
+
+
+
--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializers
+from rest_framework import viewsets
+from rest_framework import status
 
-# Create your views here.
+
+class HelloWorld(viewsets.ViewSet):
+    
+    def list(self,request):
+        users = User.objects.all()
+        serializer = UserSerializers(users,many=True)
+
+        return Response(serializer.data)
+
+    def create(self,request):
+        serializer = UserSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+        
+
+    
+
+    
